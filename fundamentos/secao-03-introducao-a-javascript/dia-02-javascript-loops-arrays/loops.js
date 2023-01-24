55dabe407bd45e186794d2dd2d4d41edb02319ae
Amanayara Slot <amanayaradev@gmail.com>
2023-01-24 19:00:12 -0300

    adicionando media do array

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
@@ -4,9 +4,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log(percorrer);
 // }
 
-let soma = 0;
+// let soma = 0;
+
+// for(let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+// console.log(soma)
+
+let media = 0;
 
 for(let index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
+    media += numbers[index] / numbers.length;
 }
-console.log(soma)
+console.log(media)
