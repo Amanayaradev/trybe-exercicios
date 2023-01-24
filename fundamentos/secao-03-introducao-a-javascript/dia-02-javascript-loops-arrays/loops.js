8d6889a02cd732c422b7dac5a57f53de518395c1
Amanayara Slot <amanayaradev@gmail.com>
2023-01-24 18:56:28 -0300

    adicionando soma

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
@@ -1,6 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let percorrer of numbers) {
-    console.log(percorrer);
-}
+// for(let percorrer of numbers) {
+//     console.log(percorrer);
+// }
 
+let soma = 0;
+
+for(let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+}
+console.log(soma)
