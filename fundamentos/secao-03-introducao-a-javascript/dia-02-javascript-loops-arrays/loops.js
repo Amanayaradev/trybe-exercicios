48313fe4772db525de3dcfe6a5db3bbf28833323
Amanayara Slot <amanayaradev@gmail.com>
2023-01-25 12:11:45 -0300

    valores impares ou não

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
@@ -18,33 +18,46 @@
             // } else {
                 //     console.log("valor menor ou igual 20")
                 // }
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-// let maiorNumero = numbers[0];
                 
-// for(let index = 1; index < numbers.length; index += 1) {
-//     if(maiorNumero < numbers[index]) {
-//         maiorNumero = numbers[index]
-//     }
-// }
-// console.log(maiorNumero)
-
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+                // let maiorNumero = numbers[0];
+                
+                // for(let index = 1; index < numbers.length; index += 1) {
+                    //     if(maiorNumero < numbers[index]) {
+                        //         maiorNumero = numbers[index]
+                        //     }
+                        // }
+                        // console.log(maiorNumero)
+                        
+                        // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+                        
 // let menorNumero = numbers[9];
                 
 // for(let index = 1; index < numbers.length; index += 1) {
-//     if(menorNumero > numbers[index]) {
-//         menorNumero = numbers[index]
-//     }
-// }
-// console.log(menorNumero)
-
-let arrayDeNumeros = [];
-for(let index = 1; index < 26; index += 1) {
-    arrayDeNumeros.push(index);
+    //     if(menorNumero > numbers[index]) {
+        //         menorNumero = numbers[index]
+        //     }
+        // }
+        // console.log(menorNumero)
+        
+        // let arrayDeNumeros = [];
+        // for(let index = 1; index < 26; index += 1) {
+            //     arrayDeNumeros.push(index);
+            // }
+            
+            // for(let divisao = 0; divisao < arrayDeNumeros.length; divisao += 1) {
+                //     console.log(arrayDeNumeros[divisao] / 2)
+                // }
+                
+                
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impares = [];
+for(let index = 0; index < numbers.length; index += 1) {
+    if(numbers[index] % 2 === 1) {
+        impares.push(numbers[index]);
+    }
 }
-
-for(let divisao = 0; divisao < arrayDeNumeros.length; divisao += 1) {
-    console.log(arrayDeNumeros[divisao] / 2)
+if(impares % 2 === 0) {
+    console.log("nenhum valor ímpar encontrado")
+} else {
+    console.log(impares)
 }
