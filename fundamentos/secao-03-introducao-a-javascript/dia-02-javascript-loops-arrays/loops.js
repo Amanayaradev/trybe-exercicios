837f59a0ed42abafeb044a49e9cf276200d20a17
Amanayara Slot <amanayaradev@gmail.com>
2023-01-24 22:10:30 -0300

    exercicio de maior numero

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
@@ -1,22 +1,30 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for(let percorrer of numbers) {
-//     console.log(percorrer);
-// }
-
+    //     console.log(percorrer);
+    // }
+    
 // let soma = 0;
 // for(let index = 0; index < numbers.length; index += 1) {
-//     soma += numbers[index];
-// }
-// console.log(soma)
+    //     soma += numbers[index];
+    // }
+    // console.log(soma)
+    
+    // let media = 0;
+    // for(let index = 0; index < numbers.length; index += 1) {
+        //     media += numbers[index] / numbers.length;
+        // }
+        // if(media > 20) {
+            //     console.log('valor maior que 20')
+            // } else {
+                //     console.log("valor menor ou igual 20")
+                // }
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let media = 0;
-
-for(let index = 0; index < numbers.length; index += 1) {
-    media += numbers[index] / numbers.length;
+let maiorNumero = numbers[0];
+                
+for(let index = 1; index < numbers.length; index += 1) {
+    if(maiorNumero < numbers[index]) {
+        maiorNumero = numbers[index]
+    }
 }
-if(media > 20) {
-    console.log('valor maior que 20')
-} else {
-    console.log("valor menor ou igual 20")
-}
+console.log(maiorNumero)
