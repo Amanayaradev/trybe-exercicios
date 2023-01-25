f1a117a99b29119e9fe4f7445a090e3db7bfcc05
Amanayara Slot <amanayaradev@gmail.com>
2023-01-24 22:38:19 -0300

    adicionando menor numero

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-loops-arrays/loops.js
@@ -18,13 +18,24 @@
             // } else {
                 //     console.log("valor menor ou igual 20")
                 // }
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let maiorNumero = numbers[0];
+                
+// for(let index = 1; index < numbers.length; index += 1) {
+//     if(maiorNumero < numbers[index]) {
+//         maiorNumero = numbers[index]
+//     }
+// }
+// console.log(maiorNumero)
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maiorNumero = numbers[0];
+let menorNumero = numbers[9];
                 
 for(let index = 1; index < numbers.length; index += 1) {
-    if(maiorNumero < numbers[index]) {
-        maiorNumero = numbers[index]
+    if(menorNumero > numbers[index]) {
+        menorNumero = numbers[index]
     }
 }
-console.log(maiorNumero)
+console.log(menorNumero)
