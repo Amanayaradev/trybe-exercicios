d8c9332da6cd168384c494b619f7263c45f7bd61
Amanayara Slot <amanayaradev@gmail.com>
2023-01-26 19:17:10 -0300

    função logar deslogar

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/index.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/index.js
@@ -92,3 +92,22 @@
 //   console.log(car[outro])
 // }
 
+let status = 'deslogado';
+
+function logarDeslogar() {
+  if (status === 'deslogado') {
+    status = 'logado';
+  } else {
+    status = 'deslogado';
+  }
+}
+
+// console.log(status); // deslogado
+
+// logarDeslogar();
+// console.log(status); // logado
+
+// logarDeslogar();
+// console.log(status); // deslogado
+
+// console.log('O usuário está ' + status); // O usuário está deslogado
