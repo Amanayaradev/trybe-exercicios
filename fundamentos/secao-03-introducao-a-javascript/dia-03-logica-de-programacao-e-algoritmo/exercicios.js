72d53ac348e8b3fce7050de4c40bffeb1b2c5761
Amanayara Slot <amanayaradev@gmail.com>
2023-01-26 08:22:21 -0300

    adicionando exercicio de inversao

--- fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/exercicios.js
@@ -8,14 +8,31 @@
 // multiplicar cada item do for
 // imprimir o resultado final
 
-// for(let index02 = 9; index02 >= 1; index02 -= 1){
-//     fatorial.push(index * index02);
+// let dez = 10;
+// let fatorial = 1;
+// for(let index = dez; index >= 1; index -= 1) {
+//     fatorial *= index;
 // }
 
-let dez = 10;
-let fatorial = 1;
-for(let index = dez; index >= 1; index -= 1) {
-    fatorial *= index;
-}
+// console.log(fatorial)
 
-console.log(fatorial)
+// Utilize a estrutura de repetição for para desenvolver um algoritmo
+// que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” 
+// seria invertida para “ananab”. Utilize a string abaixo como exemplo, 
+// depois image.pngtroque por outras para verificar se seu algoritmo está funcionando corretamente.
+// let stringInvertida = "";
+// let stringFatiada = [];
+// stringFatiada = inverter.split("");
+// let stringReverse = stringFatiada.reverse()
+// for (let i = 0; i < inverter.length; i++) {
+//     stringInvertida = stringReverse.join("")
+// }
+// console.log(stringInvertida);
+
+let inverter = 'tryber';
+let inversao = '';
+for(let index = inverter.length - 1; index >= 0; index -= 1) {
+    inversao += inverter[index];
+}
+console.log(inversao)
+
