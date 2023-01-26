118ae4a26ccf3a40bd7c5031dbd07e0015e37f23
Amanayara Slot <amanayaradev@gmail.com>
2023-01-26 20:30:41 -0300

    funcoes de operações aritmetica básica

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/funcoes.js
@@ -0,0 +1,24 @@
+function adicao(n1, n2) {
+    return n1 + n2;
+}
+ console.log(adicao(1, 2));
+
+ function subtracao(n1, n2) {
+    return n1 - n2;
+}
+ console.log(subtracao(1, 2));
+
+ function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+ console.log(multiplicacao(1, 2));
+
+ function divisao(n1, n2) {
+    return n1 / n2;
+}
+ console.log(divisao(1, 2));
+
+ function modulo(n1, n2) {
+    return n1 % n2
+}
+ console.log(modulo(1, 2));
