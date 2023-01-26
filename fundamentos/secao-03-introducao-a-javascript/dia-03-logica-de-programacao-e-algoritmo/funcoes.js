ee12f8c502a9ae88417da9ba2d5f5da78740adea
Amanayara Slot <amanayaradev@gmail.com>
2023-01-26 20:34:52 -0300

    funcao maior numero

--- fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/funcoes.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/funcoes.js
@@ -1,24 +1,35 @@
-function adicao(n1, n2) {
-    return n1 + n2;
-}
- console.log(adicao(1, 2));
+// function adicao(n1, n2) {
+//     return n1 + n2;
+// }
+//  console.log(adicao(1, 2));
 
- function subtracao(n1, n2) {
-    return n1 - n2;
-}
- console.log(subtracao(1, 2));
+//  function subtracao(n1, n2) {
+//     return n1 - n2;
+// }
+//  console.log(subtracao(1, 2));
 
- function multiplicacao(n1, n2) {
-    return n1 * n2;
-}
- console.log(multiplicacao(1, 2));
+//  function multiplicacao(n1, n2) {
+//     return n1 * n2;
+// }
+//  console.log(multiplicacao(1, 2));
 
- function divisao(n1, n2) {
-    return n1 / n2;
-}
- console.log(divisao(1, 2));
+//  function divisao(n1, n2) {
+//     return n1 / n2;
+// }
+//  console.log(divisao(1, 2));
 
- function modulo(n1, n2) {
-    return n1 % n2
+//  function modulo(n1, n2) {
+//     return n1 % n2
+// }
+//  console.log(modulo(1, 2));
+
+function maiorNumero(n1, n2) {
+    if(n1 > n2) {
+        return n1;
+    } else if (n1 < n2) {
+        return n2;
+    } else {
+        return 'são numeros iguais';
+    }
 }
- console.log(modulo(1, 2));
+console.log(maiorNumero(2, 1));
