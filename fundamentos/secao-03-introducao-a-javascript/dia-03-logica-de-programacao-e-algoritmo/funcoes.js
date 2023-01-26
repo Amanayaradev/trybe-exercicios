5afe60fa34456155245328e5cd5abc05a06188be
Amanayara Slot <amanayaradev@gmail.com>
2023-01-26 20:53:16 -0300

    funcao exibe qual maior numero de 3

--- fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/funcoes.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmo/funcoes.js
@@ -23,13 +23,26 @@
 // }
 //  console.log(modulo(1, 2));
 
-function maiorNumero(n1, n2) {
-    if(n1 > n2) {
+// function maiorNumero(n1, n2) {
+//     if(n1 > n2) {
+//         return n1;
+//     } else if (n1 < n2) {
+//         return n2;
+//     } else {
+//         return 'são numeros iguais';
+//     }
+// }
+// console.log(maiorNumero(2, 1));
+
+function maiorNumero(n1, n2, n3) {
+    if(n1 > n2 && n1 > n3) {
         return n1;
-    } else if (n1 < n2) {
+    } else if (n2 > n1 && n2 > n3) {
         return n2;
+    } else if(n3 > n2 & n3 > n1) {
+        return n3;
     } else {
         return 'são numeros iguais';
     }
 }
-console.log(maiorNumero(2, 1));
+console.log(maiorNumero(4, 4, 4));
