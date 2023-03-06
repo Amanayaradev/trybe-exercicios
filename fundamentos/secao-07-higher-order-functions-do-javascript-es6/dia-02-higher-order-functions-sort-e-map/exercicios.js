0d28f36ddb890666ca72d9b1bbef4b78fb0e5c8e
Amanayara Slot <amanayaradev@gmail.com>
2023-03-06 13:57:11 -0300

    enviando repositorios

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-02-higher-order-functions-sort-e-map/exercicios.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-02-higher-order-functions-sort-e-map/exercicios.js
@@ -161,7 +161,7 @@ const estudantes = [
 const reportStatus = (name, students) => {
     const studentInfo = students.find((student) => student.nome === name);
     return studentInfo.materias.map((materia) => (
-      `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+     `${materia.name} ${materia.nota} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
     ));
   };
   
