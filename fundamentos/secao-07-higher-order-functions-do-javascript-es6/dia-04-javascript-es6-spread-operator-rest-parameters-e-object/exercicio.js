098814a658db676b585b9259a7c327004151d9ea
Amanayara Slot <amanayaradev@gmail.com>
2023-03-07 18:44:33 -0300

    exercicio dois

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object/exercicio.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object/exercicio.js
@@ -1,10 +1,39 @@
-const rectangleArea = (width, height) => width * height;
+// const rectangleArea = (width, height) => width * height;
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((rectangle) => {
-  console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
-});
+// rectangles.forEach((rectangle) => {
+//   console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
+// });
+
+// const sum = (...num) => {
+//     return num.reduce(((inicio, segundo) => inicio + segundo), 0) 
+// }
+// console.log(sum(4, 5, 6))
+
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+const alex = {
+    name: 'Alex',
+    age: 26,
+    likes: ['fly fishing'],
+    nationality: 'Australian',
+  };
+  
+  const gunnar = {
+    name: 'Gunnar',
+    age: 30,
+    likes: ['hiking', 'scuba diving', 'taking pictures'],
+    nationality: 'Icelandic',
+  };
+  
+  // complete a assinatura da função abaixo
+  const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+  // <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+  
+  // Retornos esperados:
+  console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+  console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
